2ad6434fa4794cfa2112ca016b2cc476f43a0f15
alejandro <lopez.alejandro@dali.dartmouth.edu>
2023-07-14 12:46:43 -0400

    fixed bug with dropdown and mouse leave

--- components/dropdown/Dropdown.tsx
+++ components/dropdown/Dropdown.tsx
@@ -1,21 +1,50 @@
+import {
+  Dispatch,
+  SetStateAction,
+  useState,
+  createContext,
+  useContext
+} from "react";
 import { laptopMenu, accessoriesMenu, deskptopMenu } from "@/utils/menuItems";
-import Item from './Item';
+import Item from "./Item";
+
+interface ContextProps {
+  openType: string;
+  setOpenType: Dispatch<SetStateAction<string>>;
+}
+
+export const DropDownConext = createContext<ContextProps>({
+  openType: "",
+  setOpenType: (): string => "",
+});
 
 export default function DropDown() {
+  const [openType, setOpenType] = useState(""); // to open only one dropdown at the time
+
   return (
     <nav
-      className={"flex flex-col md:flex-row items-center justify-center w-9/12 z-10"}
+      className={
+        "flex flex-col md:flex-row items-center justify-center w-9/12 z-10 h-[4.3rem]"
+      }
+      onMouseLeave={() => setOpenType("")}
     >
       <ul
         className={
           "flex flex-col md:flex-row items-center justify-evenly w-11/12"
         }
       >
-
-        <Item menu={laptopMenu} />
-        <Item menu={deskptopMenu} />
-        <Item menu={accessoriesMenu} />
+        <DropDownConext.Provider value={{ openType, setOpenType }}>
+          <Item menu={laptopMenu} />
+        </DropDownConext.Provider>
+        <DropDownConext.Provider value={{ openType, setOpenType }}>
+          <Item menu={deskptopMenu} />
+        </DropDownConext.Provider>
+        <DropDownConext.Provider value={{ openType, setOpenType }}>
+          <Item menu={accessoriesMenu} />
+        </DropDownConext.Provider>
       </ul>
     </nav>
   );
 }
+
+export const useDropdownContext = () => useContext(DropDownConext);
--- components/dropdown/Item.tsx
+++ components/dropdown/Item.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { useState } from "react";
+import { useDropdownContext } from "./Dropdown";
 import { Menu } from "@/types/menuTypes";
 import SubItem from "./SubItem";
 import RightArrow from "@/public/arrows/right1.svg";
@@ -10,15 +11,17 @@ interface MenuProps {
 
 export default function Item(props: MenuProps) {
   const { menu } = props;
-  const [isOpen, setIsOpen] = useState(false);
-  const [activeType, setActiveType] = useState("");
+  const { openType, setOpenType } = useDropdownContext();
+  const [activeSubtype, setActiveSubtype] = useState("");
 
   return (
-    <div className={"flex flex-col"}>
-      <button onMouseEnter={() => setIsOpen(true)}>
+    <div
+      className={"flex flex-col"}
+    >
+      <button onMouseEnter={() => setOpenType(menu.title)}>
         <p
           className={`${
-            isOpen
+            openType === menu.title
               ? "text-[#AE0000] underline underline-offset-4"
               : "text-white"
           } text-xl`}
@@ -26,9 +29,9 @@ export default function Item(props: MenuProps) {
           {menu.title}
         </p>
       </button>
-      {isOpen && (
+      {openType === menu.title && (
         <div
-          onMouseLeave={() => setIsOpen(false)}
+          onMouseLeave={() => setOpenType("")}
           className={
             "bg-[#292929] absolute top-[4.2rem] flex flex-col items-start p-2 w-56"
           }
@@ -40,12 +43,12 @@ export default function Item(props: MenuProps) {
                   "flex flex-row items-center justify-between w-full hover:bg-[#AE0000] cursor-pointer p-2"
                 }
                 key={index}
-                onMouseEnter={() => setActiveType(item.title)}
+                onMouseEnter={() => setActiveSubtype(item.title)}
               >
                 <h3 className={"text-white text-lg"}>{item.title}</h3>
                 <Image src={RightArrow} alt="right arrow" />
               </div>
-              {item.submenu && activeType === item.title && (
+              {item.submenu && activeSubtype === item.title && (
                 <SubItem submenu={item.submenu} />
               )}
             </div>
